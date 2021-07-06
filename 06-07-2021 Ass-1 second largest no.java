34706a196930224915219e39296840cfb421f328
AWDCRJYMPCS49 <86506493+AWDCRJYMPCS49@users.noreply.github.com>
2021-07-06 10:41:57 +0530

    Create 06-07-2021 Ass-1 second largest no.java

---
+++ 06-07-2021 Ass-1 second largest no.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main 
+{
+   public static void main(String args[])
+   {
+      int temp,k,i,j;
+      int arr[]={1,2,3,4,5};
+      k=arr.length;
+      for(i=0;i<k;i++)
+      {
+         for(j=i+1;j<k;j++)
+         {
+           if(arr[i]>arr[j])
+           {
+               temp=arr[i];
+               arr[i]=arr[j];
+               arr[j]=temp;
+           }
+         }
+      }
+      System.out.println("second largest number is"+arr[k-2]);
+   }
+}
