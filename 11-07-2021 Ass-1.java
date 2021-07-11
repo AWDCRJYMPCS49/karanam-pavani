c6e7e3dbe4458aaad56e70448b942c1708ce3c83
AWDCRJYMPCS49 <86506493+AWDCRJYMPCS49@users.noreply.github.com>
2021-07-11 12:22:54 +0530

    Create 11-07-2021 Ass-1.java

---
+++ 11-07-2021 Ass-1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a String");
+        String str=sc.next();
+        int i;
+        char []arr=str.toCharArray();
+        for(i=arr.length-1;i>=0;i--)
+            System.out.print(arr[i]);
+    }
+}
+
+
+Output:
+Enter a String
+
+hello
+
+olleh
+
